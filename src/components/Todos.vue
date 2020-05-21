c851e280e97cb64f1d30260722c1db22228a0ddf
GC <gc@GCs-MacBook-Pro.local>
2020-05-21 00:45:16 -0400

    finished the todo project, added click to edit the task in Todo.js

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,71 +1,115 @@
 <template>
-  <div>
-    <h3>Todos</h3>
-    <div v-for="todo in allTodos" :key="todo.id" class="todo">{{ todo.title }}</div>
-  </div>
+	<div>
+		<h3>Todos</h3>
+		<div class="legend">
+			<span>Double click to mark as complete</span>
+			<span>
+				<span class="incomplete-box"></span> = Incomplete
+			</span>
+			<span>
+				<span class="complete-box"></span> = Complete
+			</span>
+		</div>
+		<div
+			@dblclick="onDbClick(todo)"
+			@click="editTodo($event, todo)"
+			v-for="todo in allTodos"
+			:key="todo.id"
+			class="todo"
+			v-bind:class="{'is-complete': todo.completed}"
+		>
+			<!-- {{ todo.title }} -->
+			<input type="text" v-bind:value="todo.title" disabled="disabled" />
+			<i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+		</div>
+	</div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 
 export default {
-  name: "Todos",
-  methods: {
-    ...mapActions(['fetchTodos'])
-  },
-  computed: mapGetters(["allTodos"]),
-  created() {
-    this.fetchTodos();
-  }
+	name: "Todos",
+	methods: {
+		...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+		onDbClick(todo) {
+			const updateTodo = {
+				id: todo.id,
+				title: todo.title,
+				completed: !todo.completed
+			};
+			this.updateTodo(updateTodo);
+		},
+		editTodo(e, todo) {
+			const isDiabled =
+				e.target.firstChild && e.target.firstChild.getAttribute("disabled");
+			if (isDiabled) {
+				e.target.firstChild.removeAttribute("disabled");
+				e.target.firstChild.focus();
+			} else {
+				const updateTodo = {
+					id: todo.id,
+					title: todo.title,
+					completed: todo.completed
+				};
+				this.updateTodo(updateTodo);
+				e.target.firstChild.setAttribute("disabled", "disabled");
+			}
+		}
+	},
+	computed: mapGetters(["allTodos"]),
+	created() {
+		this.fetchTodos();
+	}
 };
 </script>
 
 <style scoped>
 .todos {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 1rem;
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	grid-gap: 1rem;
 }
 .todo {
-  border: 1px solid #ccc;
-  background: #41b883;
-  padding: 1rem;
-  border-radius: 5px;
-  text-align: center;
-  position: relative;
-  cursor: pointer;
+	border: 1px solid #ccc;
+	background: #41b883;
+	padding: 1rem;
+	border-radius: 5px;
+	text-align: center;
+	position: relative;
+	cursor: pointer;
 }
 i {
-  position: absolute;
-  bottom: 10px;
-  right: 10px;
-  color: #fff;
-  cursor: pointer;
+	position: absolute;
+	bottom: 10px;
+	right: 10px;
+	color: #fff;
+	cursor: pointer;
 }
 .legend {
-  display: flex;
-  justify-content: space-around;
-  margin-bottom: 1rem;
+	display: flex;
+	justify-content: space-around;
+	margin-bottom: 1rem;
 }
 .complete-box {
-  display: inline-block;
-  width: 10px;
-  height: 10px;
-  background: #35495e;
+	display: inline-block;
+	width: 10px;
+	height: 10px;
+	background: #35495e;
 }
 .incomplete-box {
-  display: inline-block;
-  width: 10px;
-  height: 10px;
-  background: #41b883;
+	display: inline-block;
+	width: 10px;
+	height: 10px;
+	background: #41b883;
 }
 .is-complete {
-  background: #35495e;
-  color: #fff;
+	background: #35495e;
+	color: #fff;
 }
 @media (max-width: 500px) {
-  .todos {
-    grid-template-columns: 1fr;
-  }
+	.todos {
+		grid-template-columns: 1fr;
+	}
 }
 </style>
