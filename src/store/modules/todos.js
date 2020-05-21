12e4568bc0d67c1d437481b9aa2cef130648a5d0
GC <gc@GCs-MacBook-Pro.local>
2020-05-20 21:46:43 -0400

    first commit on vue_todo project, still setting up vuex

---
+++ src/store/modules/todos.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const state = {
+	todos: [],
+};
+
+const getters = {
+	allTodos: (state) => state.todos,
+};
+
+const actions = {
+	async fetchTodos() {
+		const response = await axios.get(
+			"https://jsonplaceholder.typicode.com/todos"
+		);
+		console.log(response.data);
+	},
+};
+
+const mutations = {};
+
+export default {
+	state,
+	getters,
+	actions,
+	mutations,
+};
